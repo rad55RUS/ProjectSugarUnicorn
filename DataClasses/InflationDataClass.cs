b6e7a0618b608cf354ae081a6ccc4656a4d81ed4
rad55RUS <radel-02@inbox.ru>
2023-05-04 15:40:29 +0300

    - Added data loading to InflationModel - Created interface for data classes

--- DataClasses/InflationDataClass.cs
+++ DataClasses/InflationDataClass.cs
@@ -1,16 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DataClasses
 {
-    public class InflationDataClass
+    public class InflationDataClass : IDataClass
     {
-        public InflationDataClass()
-        {
+        // Private fields
+        private int year;
+        private double cpi;
+        //
 
+        // Public properties
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
         }
+
+        public double CPI
+        {
+            get
+            {
+                return cpi;
+            }
+            set
+            {
+                cpi = value;
+            }
+        }
+        //
+
+        // Constructors
+        public InflationDataClass() { }
+
+        public InflationDataClass(int year, double cpi)
+        {
+            this.Year = year;
+            this.CPI = cpi;
+        }
+        //
     }
 }
--- Model/InflationModel.cs
+++ Model/InflationModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DataClasses;
 
 using MainProject.Properties;
+using System;
 
 namespace MainProject
 {
@@ -25,6 +26,27 @@ namespace MainProject
         public void LoadData(string path)
         {
             XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(path);
+            XmlElement xRoot = xDoc.DocumentElement;
+            if (xRoot != null)
+            {
+                foreach (XmlElement xnode in xRoot)
+                {
+                    InflationDataClass inflationObject = new InflationDataClass();
+
+                    XmlNode attr = xnode.Attributes.GetNamedItem("Year");
+                    inflationObject.Year = Convert.ToInt32(attr.Value);
+                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        if (childnode.Name == "CPI")
+                        {
+                            inflationObject.CPI = Convert.ToDouble(childnode.Value);
+                        }
+                    }
+
+                    InflationData.Add(inflationObject);
+                }
+            }
         }
         //
     }
